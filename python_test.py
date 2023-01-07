a6b72a6515d9772897e12b594a401c6b2ea592bf
reneweb94 <RenWeber@gmx.net>
2023-01-07 11:54:35 +0100

    Small change in file

--- python_test.py
+++ python_test.py
@@ -1,4 +1,4 @@
 ﻿name = 'James'
 age = 28
 
-print("Hello, Mr. " + name + ", I heart you are " + age + " old. Niiiccceeee!!!!")
+print("Hello, Mrs. " + name + ", I heart you are " + age + " old. Niiiccceeee!!!!")
